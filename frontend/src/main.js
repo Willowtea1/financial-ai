8de8af570314220c94eeb7144f6de1d6364daf8e
Azfar <ahmadazfarazhar@gmail.com>
2026-01-18 19:19:24 +0800

    Commit 3

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
+
+import 'vuetify/styles'
 import '@mdi/font/css/materialdesignicons.css'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
 
 const app = createApp(App)
 
